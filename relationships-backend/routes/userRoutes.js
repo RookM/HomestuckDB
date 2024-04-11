19836412bef2416ca42ae22aa03ba052d54316a3
RookM <rookmarcus79@gmail.com>
2024-04-11 16:00:47 -0400

    Lets User Get All Ships

--- relationships-backend/routes/userRoutes.js
+++ relationships-backend/routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { incrementShip } = require("../controllers/userController");
 const { getNameList } = require("../controllers/setupController");
+const { getShips } = require("../controllers/shipController");
 
 router.route("/").get(getNameList).post(incrementShip);
+router.route("/all").get(getShips);
 
 module.exports = router;
